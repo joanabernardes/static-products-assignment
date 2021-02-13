9c030a40e9d9e4d4d0d517cdf5ad831dbaabe6a3
Joana Bernardes <joan5991@stud.kea.dk>
2021-02-13 08:48:44 +0100

    with brand list, needs styling

--- js/productlist.js
+++ js/productlist.js
@@ -1,4 +1,10 @@
-const url = "https://kea-alt-del.dk/t7/api/products";
+const urlParams = new URLSearchParams(window.location.search);
+//in the URL grab id and store it's value in id
+const brandname = urlParams.get("brandname");
+// console.log(brandname);
+// document.querySelector("main>h3").textContent = brandname;
+
+const url = "https://kea-alt-del.dk/t7/api/products?brandname=" + brandname;
 
 fetch(url)
     .then(function (res) {
@@ -30,6 +36,7 @@ const template = document.querySelector("#cardTemplate").content;
         //clone it
 const copy = template.cloneNode(true);
         //change content
+copy.querySelector("img").src=`https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
 copy.querySelector(".subtle").textContent = `${product.articletype} | ${product.brandname}`;
 copy.querySelector("h3").textContent = product.productdisplayname;
 copy.querySelector(".price").textContent = product.price;
